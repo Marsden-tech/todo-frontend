e2e0494e3996d56f9507623bfabc72ec0400ca27
Marsden-tech <marsdenlwero@gmail.com>
2026-02-26 11:36:10 +0300

    add admin role guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { RegisterComponent } from './components/register/register';
 import { TodoComponent } from './components/todo/todo';
 import { AdminComponent } from './components/admin/admin';
 import { authGuard } from './guards/auth-guard';
+import { adminGuard } from './guards/admin-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'todos', component: TodoComponent, canActivate: [authGuard] },
-  { path: 'admin', component: AdminComponent, canActivate: [authGuard] },
+  { path: 'admin', component: AdminComponent, canActivate: [authGuard, adminGuard] },
   { path: '**', redirectTo: 'login' }
 ];
